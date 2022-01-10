3991436e5061ec1708fee393e71979219798006c
Daniel Martinez-Gamboa <dmartinezgamboa@gmail.com>
2022-01-09 17:05:48 -0800

    Add link following capability to crawler

--- main/tutorial/tutorial/spiders/quotes_spider.py
+++ main/tutorial/tutorial/spiders/quotes_spider.py
@@ -6,7 +6,6 @@ class QuotesSpider(scrapy.Spider):
 
   start_urls = [
     'http://quotes.toscrape.com/page/1/',
-    'http://quotes.toscrape.com/page/2/',
   ]
 
   def parse(self, response):
@@ -16,3 +15,7 @@ class QuotesSpider(scrapy.Spider):
         'author': quote.css('small.author::text').get(),
         'tags': quote.css('div.tags a.tag::text').getall(),
       }
+
+    next_page = response.css('li.next a::attr(href)').get()
+    if next_page is not None:
+      yield response.follow(next_page, callback=self.parse)
